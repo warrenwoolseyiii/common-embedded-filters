69940059dbe3c12a79c56e234ccf81a36c1dfee9
budgettsfrog <budgettsfrog@protonmail.com>
2024-02-07 14:56:36 -0800

    Renaming and moving many files in preparation for documentation

--- impl/fir_filter/fir_config.h
+++ impl/fir_filter/fir_config.h
@@ -4,9 +4,9 @@
 #ifndef FIR_CONFIG_H_
 #define FIR_CONFIG_H_
 #include "../fixed_point.h"
-#define FIR_NUM_COEFFS   31
-#define FIR_FILTER_ORDER 31
-#define FIR_START_FREQ   12.5
-#define FIR_STOP_FREQ    0
+#define FIR_NUM_COEFFS   0 // Insert the number of FIR coefficients here
+#define FIR_FILTER_ORDER 0 // Insert the FIR filter order here
+#define FIR_START_FREQ   0 // Insert the start frequency of the bandpass filter here
+#define FIR_STOP_FREQ    0 // Insert the stop frequency of the bandpass filter here
 extern filter_coeff_t _fir_b_coeffs[FIR_NUM_COEFFS];
 #endif // ifndef FIR_CONFIG_H_
