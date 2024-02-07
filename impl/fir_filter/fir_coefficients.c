1ec3553f8e2e6950f44632841d95ef47c6d5fdd1
budgettsfrog <budgettsfrog@protonmail.com>
2024-02-07 15:09:31 -0800

    removed cached versions of the config files

--- impl/fir_filter/fir_coefficients.c
+++
@@ -1,6 +0,0 @@
-// This is an auto generated file by filter_designer.py
-// You can modify this manually or regenerate it by running filter_designer.py
-// See the README for more information
-#include "fir_config.h"
-filter_coeff_t _fir_b_coeffs[FIR_NUM_COEFFS] =
-{ /* Insert FIR coefficients here*/ };
