1ba84d70599c7c58206d11c9e5023c40b3a496ff
budgettsfrog <budgettsfrog@protonmail.com>
2024-04-12 11:05:12 -0700

    support integer types

--- impl/filter_types.h
+++ impl/filter_types.h
@@ -26,16 +26,22 @@
 #include <stdint.h>
 #include <stddef.h>
 
-// Define FILTER_USE_FP_MATH at compile time to utilize floating point math, otherwise
-// we default to fixed point math.
-#if defined(FILTER_USE_FP_MATH)
+// Define FILTER_USE_FLOAT_MATH at compile time to utilize floating point math.
+// Define FILTER_USE_FIXED_LIB at compile time to utilize a builtin fixed point math library.
+#if defined(FILTER_USE_FLOAT_MATH)
+#undef FILTER_USE_FIXED_LIB
 typedef double        filter_coeff_t;
 typedef float         filter_data_t;
 typedef double        filter_accum_t;
-#else
+#elif defined(FILTER_USE_FIXED_LIB)
 typedef long _Accum   filter_coeff_t;
 typedef _Accum        filter_data_t;
 typedef long _Accum   filter_accum_t;
+#else
+#warning "No fractional math defined, this could result in incorrect coefficients being generated."
+typedef long        filter_coeff_t;
+typedef int         filter_data_t;
+typedef long        filter_accum_t;
 #endif /* FILTER_USE_FP_MATH */
 
 #endif /* FILTER_TYPES_H_ */
